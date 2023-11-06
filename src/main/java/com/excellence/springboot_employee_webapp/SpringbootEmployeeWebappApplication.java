792f7a74af7d1251f4839010ba3c664fdb30a055
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-06 10:40:09 +0530

    base-code-created

---
+++ src/main/java/com/excellence/springboot_employee_webapp/SpringbootEmployeeWebappApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot_employee_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeeWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/springboot_employee_webapp/SpringbootEmployeeWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot_employee_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmployeeWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
